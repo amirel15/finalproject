68f9b04e87cf3f68569086249cbba4a47432b7bc
ashle <amireles1334@yahoo.com>
2023-04-23 14:04:53 -0500

    added and configured Login & useauth part 2

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,18 +1,23 @@
 <script setup>
 import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute} from 'vue-router'
 
 import { useAuth } from '../composables/useAuth'
 
 const { login, logout } = useAuth()
 const router = useRouter()
+const route = useRoute()
 
 const username = ref('')
 const password = ref('')
 
 const logUserIn = () => {
   if (login(username.value, password.value)) {
-    router.push({name: 'Home'})
+    if (route.query.redirect) {
+      router.push(route.query.redirect)
+    } else {
+      router.push({ name: 'Home' })
+    }
   } else {
     logout()
   }
@@ -20,18 +25,21 @@ const logUserIn = () => {
 </script>
 
 <template>
-  <form class="loginForm" @submit.prevent="logUserIn">
+  <form class="login-form" @submit.prevent="logUserIn">
   <input v-model="username" type="text" placeholder="Username"/>
   <input v-model="password" type="password" placeholder="Password"/>
-  <button type="submit" class="bg-green-500 px-4 py-2">Click to Login</button>
+  <button type="submit" class="bg-green-500 font-semibold px-4 py-2">LOGIN</button>
   </form>
 </template>
 
 <style scoped lang="postcss">
-.loginForm {
-  @apply flex max-w-lg mx-auto flex-col mt-80 gap-4 bg-white p-8 rounded-md shadow-lg;
-  &input {
-    @apply text-xl pg-4 py-2 ring-1 rounded-md ring-slate-500;
+.login-form {
+  @apply mx-auto mt-20 flex max-w-lg flex-col gap-4 rounded-md bg-blue-200 p-8 shadow-lg;
+  & input {
+    @apply rounded-md px-4 py-2 text-xl ring-1 ring-slate-500 text-center italic;
+  }
+  & button {
+    @apply rounded-md
   }
 }
 </style>
